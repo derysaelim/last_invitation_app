97bc8ce78b3abe5ad723f9d61d0be4dc3cf94fb2
derysaelim <derysaelim@gmail.com>
2025-03-01 00:20:36 +0700

    update project

--- lib/view/login/login_page.dart
+++ lib/view/login/login_page.dart
@@ -10,6 +10,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffFFFFFF),
       body: SizedBox(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
